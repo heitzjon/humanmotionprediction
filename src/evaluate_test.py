07466132bc6602e6f94f9104e5a82649729b8492
Jonas <jonas@clt-mob-t-6301.zhaw.ch>
2018-05-30 16:42:10 +0200

    changes for model evaluations

--- src/evaluate_test.py
+++ src/evaluate_test.py
@@ -63,8 +63,11 @@ def main(config):
                 #   3) fetch both the final state and prediction of the RNN model that are then re-used in the next
                 #      iteration
 
-                fetch = None
-                feed_dict = None
+
+                fetch = [state]
+                feed_dict = {placeholders['input_pl']: next_pose,
+                         placeholders['seq_lengths_pl']: batch.seq_lengths}
+
 
                 [state, predicted_pose] = sess.run(fetch, feed_dict)
 
